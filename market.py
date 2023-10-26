6232c4f4d2b1fd1cfee1cc6a254be2e0ef55400e
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-10-25 21:51:11 -0500

    Sending data to Templates

--- market.py
+++ market.py
@@ -6,4 +6,13 @@ app = Flask(__name__)
 @app.route("/home")
 def home_page():
     return render_template('home.html')
+
+@app.route("/market")
+def market_page():
+    items = [
+        {'id': 1, 'nombre': 'Telefono', 'barcode': '893212299897', 'precio': 500},
+        {'id': 2, 'nombre': 'Laptop', 'barcode': '123985473|65', 'precio': 900},
+        {'id': 3, 'nombre': 'Teclado', 'barcode': '231985128446', 'precio': 150},
+    ]
+    return render_template('market.html', items=items)
     
