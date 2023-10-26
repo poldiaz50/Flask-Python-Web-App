add45afe22852674722487a49471bfb7811ae20f
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-10-26 00:58:31 -0500

    Models and Databases

--- market.py
+++ market.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+##contexto
+app.app_context().push()
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    nombre = db.Column(db.String(length=30), nullable=False, unique=True)
+    precio = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    descripcion = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+     return f'Item{self.nombre}'
 
 @app.route("/")
 @app.route("/home")
@@ -9,10 +25,6 @@ def home_page():
 
 @app.route("/market")
 def market_page():
-    items = [
-        {'id': 1, 'nombre': 'Telefono', 'barcode': '893212299897', 'precio': 500},
-        {'id': 2, 'nombre': 'Laptop', 'barcode': '123985473|65', 'precio': 900},
-        {'id': 3, 'nombre': 'Teclado', 'barcode': '231985128446', 'precio': 150},
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
     
